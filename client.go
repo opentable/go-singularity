30456ff2c8ae29549dff60c838ae6d979ed35195
Judson <nyarly@gmail.com>
2016-07-18 16:17:02 -0700

    Update to test_with_docker

--- client.go
+++ client.go
@@ -11,10 +11,16 @@ import (
 // Client is the top level singularity client.
 // Wraps the swaggering GenericClient
 type Client struct {
-	swaggering.GenericClient
+	swaggering.Requester
 }
 
 // NewClient builds a new Client
 func NewClient(apiBase string) (client *Client) {
-	return &Client{swaggering.GenericClient{apiBase, swaggering.NullLogger{}, http.Client{}}}
+	return &Client{&swaggering.GenericClient{apiBase, swaggering.NullLogger{}, http.Client{}}}
+}
+
+// NewDummyClient builds a client/control pair for testing
+func NewDummyClient(apiBase string) (*Client, swaggering.DummyControl) {
+	sc, ctrl := swaggering.NewChannelDummy()
+	return &Client{&sc}, ctrl
 }
