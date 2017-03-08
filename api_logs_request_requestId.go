2fc86541d683771750495b03f16d9273ceee83d9
Judson <nyarly@gmail.com>
2017-03-08 11:37:33 -0800

    Adds support for S3Logs

--- api_logs_request_requestId.go
+++ api_logs_request_requestId.go
@@ -1,1 +1,18 @@
 package singularity
+
+import "github.com/opentable/go-singularity/dtos"
+
+func (client *Client) GetS3LogsForRequest(requestId string, start int64, end int64) (response dtos.SingularityS3LogList, err error) {
+	pathParamMap := map[string]interface{}{
+		"requestId": requestId,
+	}
+
+	queryParamMap := map[string]interface{}{
+		"start": start, "end": end,
+	}
+
+	response = make(dtos.SingularityS3LogList, 0)
+	err = client.DTORequest(&response, "GET", "/api/logs/request/{requestId}", pathParamMap, queryParamMap)
+
+	return
+}
